63a088b70ddb2cf917d8a26182b8a5736a28856a
Thien Trinh <thientd87@gmail.com>
2022-03-07 10:11:01 +0700

    Add MOPA WEB

---
+++ Mopa.Cms.Web/Models/StandardPage.cs
@@ -0,0 +1,10 @@
+using Piranha.AttributeBuilder;
+using Piranha.Models;
+
+namespace Mopa.Cms.Web.Models
+{
+    [PageType(Title = "Standard page")]
+    public class StandardPage  : Page<StandardPage>
+    {
+    }
+}
